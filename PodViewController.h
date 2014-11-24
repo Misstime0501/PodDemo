6447e7f55ecb424cd09db2e96537c346f953238d
Misstime0501 <wolichen0501@gmail.com>
2014-11-24 10:14:19 +0800

    上传

---
+++ PodViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectForPodsDemo
+//
+//  Created by HRWY on 14/11/20.
+//  Copyright (c) 2014年 HRWY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PodViewController : UIViewController
+
+
+@end
+
---
+++ PodViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProjectForPodsDemo
+//
+//  Created by HRWY on 14/11/20.
+//  Copyright (c) 2014年 HRWY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PodViewController ()
+
+@end
+
+@implementation PodViewController
+
+- (void)viewDidLoad
+{
+    NSLog(@"Hello world At ProjectOfPodsDemo ViewController!!");
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
